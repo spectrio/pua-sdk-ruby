aaf5b2247f6d9901e5efe663aef85fc792c97339
Peter Karman <karpet@peknet.com>
2014-09-26 23:13:59 -0500

    mmmm. color.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ Dotenv.load
 RSpec.configure do |config|
   #config.run_all_when_everything_filtered = true
   #config.filter_run :focus
+  config.color = true
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
