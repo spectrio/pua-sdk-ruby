ed715e3c23c6f9735fc17eb7fbe710339c5460fa
Peter Karman <karpet@peknet.com>
2014-09-26 23:20:23 -0500

    more tests (some failing due to #588)

--- spec/lib/popuparchive-client_spec.rb
+++ spec/lib/popuparchive-client_spec.rb
@@ -44,7 +44,34 @@ describe "collections" do
     expect(resp.collections.size).to eq(resp.collections.size)
     expect(resp.collections[0].title).to eq('My Uploads') # TODO always true?
     expect(resp.collections[0].storage).to be_truthy
+
+    # specific collection
+    coll = client.get('/collections/'+resp.collections[0].id.to_s)
+    #puts pp(coll)
+    expect(coll.title).to eq (resp.collections[0].title)
+
+    # idiomatic
+    coll_i = client.get_collection(resp.collections[0].id.to_s)
+    expect(coll.title).to eq (coll_i.title)
+
   end
+
+  it "should fetch items and collection/items" do
+    client = get_pua_client
+    resp = client.get('/collections')
+    resp.collections.each do |coll|
+      items = client.get("/collections/#{coll.id.to_s}/items")
+      puts pp items.items
+      items.items.each do |item|
+        # request directly
+        puts "fetch item #{item.id.to_s}"
+        item_d = client.get("/items/#{item.id.to_s}")
+        if item_d.is_success
+          expect(item_d.title).to eq(item.title)
+        end
+      end
+    end
+  end 
 
 end
 
